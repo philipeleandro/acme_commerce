0d5f4b18367a3cfef87f522508e0994c0527dc36
Philipe <philipe-leandro@hotmail.com>
2022-06-12 18:40:13 -0300

    Add create function to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,26 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index, :new, :create]
 
   def index
     @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    
+    if @category.save
+      redirect_to categories_path, notice: 'Categoria cadastrada'
+    else
+      flash.now[:notice] = 'Não foi possível cadastrar'
+      render 'new'
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
---
+++ spec/system/category/user_registers_a_category_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'User registers a category' do
+  it 'and must log in' do
+    visit new_category_path
+
+    expect(current_path).to eq new_user_session_path
+  end
+
+  it 'in new_category_path' do
+    user = User.create(email: 'example@example.com', password: 'password')
+
+    visit root_path
+    click_on 'Entrar'
+    login_as(user)
+    click_on 'Log in'
+    click_on 'Categorias'
+    click_on 'Nova Categoria'
+
+    expect(page).to have_field 'Nome'
+  end
+
+  it 'success' do
+    user = User.create(email: 'example@example.com', password: 'password')
+
+    login_as(user)
+    visit new_category_path
+    fill_in 'Nome', with: 'Bag'
+    click_on 'Cadastrar'
+
+    expect(page).to have_content 'Categoria cadastrada'
+    expect(page).to have_content 'Bag'
+  end
+
+  it 'fail with empty data' do
+    user = User.create(email: 'example@example.com', password: 'password')
+
+    login_as(user)
+    visit new_category_path
+    fill_in 'Nome', with: ''
+    click_on 'Cadastrar'
+
+    expect(page).to have_content 'Não foi possível cadastrar'
+  end
+end
