7c9119e1e744dd7248a73923208770ba3f4cd5ba
Philipe <philipe-leandro@hotmail.com>
2022-06-12 17:47:57 -0300

    Create category model

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe '#valid?' do
+    context "when presence" do
+      it 'false when name is empty' do
+        category = Category.new(name: '')
+
+        expect(category.valid?).to eq false
+      end
+
+      it 'true when has a name' do
+        category = Category.new(name: 'Case')
+
+        expect(category.valid?).to eq true
+      end
+    end
+  end
+end
