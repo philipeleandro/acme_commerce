d45bfdc246688916c293365b83ff13dbebd3e056
Philipe <philipe-leandro@hotmail.com>
2022-06-13 11:27:11 -0300

    Refactor test

--- spec/system/product/user_deletes_a_product_spec.rb
+++ spec/system/product/user_deletes_a_product_spec.rb
@@ -17,22 +17,4 @@ describe 'User deletes a product' do
     expect(page).not_to have_content 'Case'
     expect(page).not_to have_content 'Phone'
   end  
-
-  # it 'does not delete others' do
-  #   user = User.create(email: 'example@example.com', password: 'password')
-  #   Client.create(name: 'Pedrinho', address: 'Rua um dia vai, 1234', state: 'Minas Gerais', city: 'Belo Horizonte')
-  #   Client.create(name: 'Pedro', address: 'Rua um dia, 134', state: 'São Paulo', city: 'São Paulo')
-    
-  #   visit root_path
-  #   click_on 'Entrar'
-  #   login_as(user)
-  #   click_on 'Log in'
-  #   click_on 'Clientes'
-  #   click_on 'Pedrinho'
-  #   click_on 'Deletar'
-
-  #   expect(page).to have_content 'Cliente deletado com sucesso'
-  #   expect(page).to have_content 'Pedro'
-  #   expect(page).to have_content 'Rua um dia, 134, São Paulo - São Paulo'
-  # end  
 end
