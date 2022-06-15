f3fdba4e381ab6ab3b230020e62ccfd9f7df72d8
Philipe <philipe-leandro@hotmail.com>
2022-06-14 22:04:51 -0300

    Add validations to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,13 @@
 class Order < ApplicationRecord
   validates :reference_number, :payment_date, :status, presence: true
+  enum status: { sent: 0, waiting: 1, production: 2, canceled: 3, delivered: 4, posting: 5 }
 
   belongs_to :client
+
+  before_validation :generate_code
+  attribute :status, default: 2
+
+  def generate_code
+    self.reference_number = SecureRandom.random_number(9999999999999)
+  end
 end
