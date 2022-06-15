859565c9503ef857d95ac744add500e141005657
Philipe <philipe-leandro@hotmail.com>
2022-06-14 22:01:23 -0300

    Refactor test

--- spec/system/order/user_views_orders_spec.rb
+++ spec/system/order/user_views_orders_spec.rb
@@ -12,14 +12,14 @@ describe 'User views orders' do
     new_client = Client.create(name:'Pedro Gomes', state: 'Ceará', city: 'Fortaleza', address: 'Rua das lutas, 1000')
     new_category = Category.create(name: 'Phone')
     new_product = Product.create(name: 'Case', value: 10, base_value: 7, image_url: 'http://www.example.com', category: new_category)
-    new_order = Order.create(reference_number: '1236545', status: 1, payment_date: Date.today, client: new_client)
+    new_order = Order.create(status: 1, payment_date: Date.today, client: new_client)
     order_product = OrderProduct.create(order_id: new_order.id, product_id: new_product.id)
 
     login_as(user)
     visit root_path
     click_on 'Ordens'
 
-    expect(page).to have_content 'Ordem: 1236545'
+    expect(page).to have_content "Ordem: #{new_order.reference_number}"
     expect(page).to have_content 'Valor: R$ 10.00'
   end
 
